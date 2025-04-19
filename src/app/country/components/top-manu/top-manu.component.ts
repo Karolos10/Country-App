39a3222fd17df5f13f7b5e23f1a19f745d782723
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-19 14:05:44 -0500

    Added TopMenu component with navigation links: - Created a responsive navigation menu using TailwindCSS and DaisyUI. - Added router links for "By Capital," "By Country," and "By Region" pages. - Included SVG icons for each menu item to enhance visual appeal. - Styled the menu with a horizontal layout and rounded corners.

---
+++ src/app/country/components/top-manu/top-manu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'country-top-manu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './top-manu.component.html',
+})
+export class TopManuComponent { }
--- src/app/country/layouts/countryLayout/countryLayout.component.ts
+++ src/app/country/layouts/countryLayout/countryLayout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopManuComponent } from "../../components/top-manu/top-manu.component";
 
 @Component({
   selector: 'app-country-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopManuComponent],
   templateUrl: './countryLayout.component.html',
 })
 export class CountryLayoutComponent { }
