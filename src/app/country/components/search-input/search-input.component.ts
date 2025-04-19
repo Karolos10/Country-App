5805afc37094523cd8ae4f8884da32f601e43a1d
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-19 14:49:18 -0500

    Refactored ByCapitalPage with reusable components: - Created  to handle search input and emit search values. - Added placeholder binding and event emitters for dynamic search functionality. - Created  as a placeholder for displaying country data. - Integrated  and  into ByCapitalPage.

---
+++ src/app/country/components/search-input/search-input.component.ts
@@ -0,0 +1,13 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'country-search-input',
+  imports: [],
+  templateUrl: './search-input.component.html',
+})
+export class SearchInputComponent {
+
+  placeHolder = input('Search');
+
+  value = output<string>();
+}
