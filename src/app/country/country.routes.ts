c2114a2cdc3c0cbbabfc01cebd452c0281cd2fac
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-19 13:22:27 -0500

    Added ByCapitalPage component: - Generated the ByCapitalPage component under the country/pages directory. - Added a basic HTML template with placeholder text.

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from "@angular/router";
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { CountryLayoutComponent } from "./layouts/countryLayout/countryLayout.component";
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital',
+        component: ByCapitalPageComponent,
+      },
+
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      }
+    ]
   }
 ];
 
