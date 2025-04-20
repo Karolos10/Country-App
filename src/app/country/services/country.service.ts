0069a59fd42c5c2bb2f960daabd0eecfcdba8115
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-19 19:07:45 -0500

    Implemented country details page with dynamic routing: - Added `searchByCountryByCode` method in `CountryService` to fetch country details by code. - Created `CountryPageComponent` to display country details dynamically based on the route parameter. - Integrated `rxResource` in `CountryPageComponent` for reactive data fetching and state management. - Updated template to handle error states and display country details when available. - Added fallback message for "Country not found" with a return link for better user experience.

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -47,4 +47,20 @@ export class CountryService {
       )
   }
 
+  searchByCountryByCode(code: string) {
+    const url = `${API_URL}/alpha/${code}`;
+
+    return this.http.get<RESTCountry[]>(url)
+      .pipe(
+        map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        map((countries) => countries.at(0)),
+        catchError((error) => {
+          console.log('Error fetching countries by capital:', error);
+
+          return throwError(() => new Error
+            (`No country was found with the codes ${code}`));
+        })
+      )
+  }
+
 }
