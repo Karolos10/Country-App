7de8da7928474f45076165550301a9d9a5250ff2
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-21 16:36:55 -0500

    Added debounce effect for input handling: - Implemented debounceEffect to delay emitting input values for a specified debounce time. - Used onCleanup to clear timeouts and ensure proper resource management. - Integrated (keyup) event to update inpuntValue dynamically for smoother user interactions.

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -10,4 +10,18 @@ export class SearchInputComponent {
   placeHolder = input('Search');
 
   value = output<string>();
+  debounceTime = signal(300);
+
+  inpuntValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup) => {
+    const value = this.inpuntValue();
+    const timeout = setTimeout(() => {
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout);
+    })
+  })
 }
